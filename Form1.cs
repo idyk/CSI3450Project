266f22534a5fc0051b331a89e516b9cee5548003
idyk <98283153+idyk@users.noreply.github.com>
2022-03-18 16:26:13 -0400

    initial build
    all files, zip file included to allow for easy access

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿//CSI3450 TEAM SORT AND DESTROY
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_UI_Maybe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Username_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guest_Click(object sender, EventArgs e)
+        {
+            Form Form2 = new Form2();
+            this.Hide();
+            Form2.Show();
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
